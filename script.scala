fc256ec650785b9e258ba4e7c069cf0a888965fd
koufounak <koufounakis1@hotmail.com>
2019-01-02 22:14:57 +0200

    Added covariance

--- script.scala
+++ script.scala
@@ -1,16 +1,25 @@
 import org.apache.spark.mllib.clustering.{KMeans, KMeansModel}
+import org.apache.spark.mllib.stat.{MultivariateStatisticalSummary, Statistics}
 import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.mllib.linalg.Matrix
+import org.apache.spark.mllib.linalg.distributed.RowMatrix
 
 val conf = new SparkConf().setAppName("Simple Application")
 val sc = new SparkContext(conf)
 // Load and parse the data
-val data = sc.textFile("/home/christos/Documents/spark-kmeans/data.csv")
+val data = sc.textFile("/home/dimitris/Desktop/spark/spark-kmeans/data.csv")
 val parsedData = data.map(s => Vectors.dense(s.split(',').map(_.toDouble))).cache()
-parsedData.collect().foreach(println)
+val mat: RowMatrix = new RowMatrix(parsedData)
+//parsedData.collect().foreach(println)
 // Cluster the data into two classes using KMeans
 val numClusters = 5
-val numIterations = 5
+val numIterations = 20
 val clusters = KMeans.train(parsedData, numClusters, numIterations)
+
+println("@@@@@@@@@@@@@@@")
+println("Print each point which class belongs")
+clusters.predict(parsedData).foreach(println)
+println("@@@@@@@@@@@@@@@")
 clusters.clusterCenters(0)
 clusters.clusterCenters(1)
 clusters.clusterCenters(2)
@@ -19,7 +28,16 @@ clusters.clusterCenters(4)
 // Evaluate clustering by computing Within Set Sum of Squared Errors
 val WSSSE = clusters.computeCost(parsedData)
 println("Within Set Sum of Squared Errors = " + WSSSE)
-parsedData.foreach(x=>{println(x(1))})//prints collumn 1//
+
+
+val summary: MultivariateStatisticalSummary = Statistics.colStats(parsedData)
+println(summary.mean)  // a dense vector containing the mean value for each column
+println("--------------------------")
+println("-----COVARIANCE MATRIX----")
+println("--------------------------")
+val cov: Matrix = mat.computeCovariance()
+//println(summary.numNonzeros)  // number of nonzeros in each column
+//parsedData.foreach(x=>{println(x(1))})//prints collumn 1//
 
 // Save and load model
 //clusters.save(sc, "target/org/apache/spark/KMeansExample/KMeansModel")
